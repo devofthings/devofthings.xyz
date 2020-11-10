c7a82e2619a00822e62be75f996ab9fbe87497d5
Chris Winter <chris-winter@live.de>
2020-11-10 22:12:06 +0100

    added a typewriter

---
+++ js/typewriter.js
@@ -0,0 +1,5 @@
+const instance = new Typewriter('#typewriter', {
+    strings: ['Hello World 👋🏼!', 'My name is Christopher Winter', "I'm a Software Developer"],
+    autoStart: true,
+    loop: true,
+  });
